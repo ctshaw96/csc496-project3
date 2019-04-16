32e7acbcb88bebb04c1012aa938f8acbe4b5952b
Christopher Shaw <chris@Christophers-MacBook-Pro.local>
2019-04-16 19:40:32 -0400

    Adding MongoDb connection

--- csc496-shortener/src/main/java/edu/wcupa/csc496/core/Database.java
+++ csc496-shortener/src/main/java/edu/wcupa/csc496/core/Database.java
@@ -2,29 +2,21 @@ package edu.wcupa.csc496.core;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
     private static final Logger LOG = LoggerFactory.getLogger(Database.class);
     private static final String HOST = "mongodb";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "admin";
     private static final int PORT = 27017;
     public static Map<String, String> db = new HashMap();
 
     public static void testDB() {
-        MongoCredential credential = MongoCredential.createCredential(USERNAME, "urls", PASSWORD.toCharArray());
-        MongoClient mongoClient = new MongoClient(new ServerAddress(HOST, PORT),
-                Arrays.asList(credential));
+        MongoClient mongoClient = new  MongoClient(HOST, PORT);
         DB database = mongoClient.getDB("url-shortener");
-
 
         //Print out DBs
         mongoClient.getDatabaseNames().forEach(System.out::println);
