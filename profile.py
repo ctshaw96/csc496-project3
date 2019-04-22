062ee0a83a28ce8520ee5117ca7696bcffd461f7
ctshaw96 <ctshaw96@gmail.com>
2019-04-22 17:11:56 -0400

    Use the ubuntu profile.py

--- profile.py
+++ profile.py
@@ -13,16 +13,7 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides the template for a full research cluster with head node, scheduler, compute nodes, and shared file systems.
-First node (head) should contain: 
-- Shared home directory using Networked File System
-- Management server for SLURM
-Second node (metadata) should contain:
-- Metadata server for SLURM
-Third node (storage):
-- Shared software directory (/software) using Networked File System
-Remaining three nodes (computing):
-- Compute nodes  
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
 """
 
 #
@@ -36,27 +27,17 @@ prefixForIP = "192.168.1."
 
 link = request.LAN("lan")
 
-i=0
-
 node = request.XenVM("head")
+node.cores = 4
+node.ram = 4096
 node.routable_control_ip = "true"
-    
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-
-iface = node.addInterface("if" + str(i))
+ 
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  
+iface = node.addInterface("if0")
 iface.component_id = "eth1"
-iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+iface.addAddress(pg.IPv4Address(prefixForIP + str(0), "255.255.255.0"))
 link.addInterface(iface)
   
-node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
-node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
-node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_docker.sh"))
-node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_docker.sh"))
-  
-# This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
-# node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
-# node.addService(pg.Execute(shell="sh", command="sudo -H -u lngo bash -c '/local/repository/ssh_setup.sh'"))
-node.addService(pg.Execute(shell="sh", command="sudo su lngo -c 'cp /local/repository/source/* /users/lngo'"))
-  
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/passwordless.sh"))
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
